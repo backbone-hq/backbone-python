3174488cca7a06eb69c6e9870147188ade312603
David Al Mjali <david.al.mjali@gmail.com>
2021-03-06 10:29:54 -0800

    Defining CLI docstring; Implementing primitive cryptographic derivations

--- kryptos/__init__.py
+++ kryptos/__init__.py
@@ -1,5 +1,40 @@
-# Define functions here
+from typing import Optional
+from nacl import exceptions, hash, pwhash, secret, public
+
+import gc
+
+__version__ = 0
 
 
-def main():
-    exit(-1)
+def derive_symmetric_key(username: str, password: str) -> bytes:
+    return pwhash.argon2i.kdf(size=32, password=password.encode(), salt=bytes.fromhex(hash.blake2b(username.encode(), 16).decode()))
+
+
+def generate_user_keypair(username: str, password: str) -> (bytes, bytes):
+    # Sensitive
+    private: public.PrivateKey = public.PrivateKey.generate()
+    symmetric_key: bytes = derive_symmetric_key(username, password)
+
+    # Public
+    hidden_key: bytes = secret.SecretBox(key=symmetric_key).encrypt(private.encode())
+    public_key: bytes = private.public_key.encode()
+
+    # Delete references to the private and symmetric keys and force garbage collection
+    del symmetric_key, private
+    gc.collect()
+
+    return public_key, hidden_key
+
+
+def decrypt_hidden_key(hidden_key: bytes, username: str, password: str) -> Optional[public.PrivateKey]:
+    symmetric_key: bytes = derive_symmetric_key(username, password)
+    private_key: bytes = secret.SecretBox(key=symmetric_key).decrypt(hidden_key)
+
+    # Delete references to the symmetric key
+    del symmetric_key
+    gc.collect()
+
+    try:
+        return public.PrivateKey(private_key)
+    except exceptions.CryptoError:
+        return None
--- kryptos/__main__.py
+++ kryptos/__main__.py
@@ -1,4 +1,34 @@
-from kryptos import main
+"""Kryptos
+
+Usage:
+    kryptos config set workspace <workspace>
+    kryptos config set token <token>
+    kryptos config nuke
+
+    kryptos (authenticate|login) <workspace> <username>
+
+    kryptos entry set <key> <value>
+    kryptos entry remove <key>
+
+    kryptos namespace create <key>
+    kryptos namespace remove <key>
+    kryptos namespace rotate <key>
+
+    kryptos allow <username> <key>
+    kryptos deny <username> <key>
+
+    kryptos user create <name> [email] [permissions]...
+    kryptos user delete <name>
+
+    kryptos --help
+    kryptos --version
+
+Options:
+    --help      Display this help message
+    --version   Display the Kryptos version
+"""
+
+# Adhere to the XDG Directory Spec: http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
 
 if __name__ == "__main__":
-    main()
+    exit(0)
