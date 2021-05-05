3518d5db44e58c30213efd10b93f8cd65f9f52ca
David Al Mjali <david.al.mjali@gmail.com>
2021-05-05 11:34:58 -0700

    Disabling CLI

--- kryptos/cli.py
+++ kryptos/cli.py
@@ -41,73 +41,77 @@ def panic(*args, **kwargs):
 
 
 def execute(arguments):
-    if KRYPTOS_CONFIG.exists():
-        with open(KRYPTOS_CONFIG) as config_file:
-            kryptos_config = {k: str(v) for k, v in json.load(config_file).items() if k in SUPPORTED_CONFIG_KEYS}
-    else:
-        kryptos_config = {}
+    pass
 
-    if arguments["config"]:
-        if arguments["list"]:
-            for key, value in kryptos_config.items():
-                if key not in SUPPORTED_CONFIG_KEYS:
-                    del kryptos_config[key]
-                    continue
+"""
+if KRYPTOS_CONFIG.exists():
+    with open(KRYPTOS_CONFIG) as config_file:
+        kryptos_config = {k: str(v) for k, v in json.load(config_file).items() if k in SUPPORTED_CONFIG_KEYS}
+else:
+    kryptos_config = {}
 
-                print(f"{key}: {value}")
-        elif arguments["get"]:
-            print(f"{arguments['<key>']}: {kryptos_config.get(arguments['<key>'])}")
-        elif arguments["set"]:
-            key = arguments["<key>"].lower()
+if arguments["config"]:
+    if arguments["list"]:
+        for key, value in kryptos_config.items():
             if key not in SUPPORTED_CONFIG_KEYS:
-                panic(f"Unsupported config key: {key}")
+                del kryptos_config[key]
+                continue
 
-            kryptos_config[key] = str(arguments["<value>"])
-        elif arguments["reset"]:
-            kryptos_config = {}
-    elif arguments["authenticate"]:
-        workspace = arguments["--workspace"] or kryptos_config.get("workspace")
-        if not workspace:
-            panic("No workspace defined")
+            print(f"{key}: {value}")
+    elif arguments["get"]:
+        print(f"{arguments['<key>']}: {kryptos_config.get(arguments['<key>'])}")
+    elif arguments["set"]:
+        key = arguments["<key>"].lower()
+        if key not in SUPPORTED_CONFIG_KEYS:
+            panic(f"Unsupported config key: {key}")
 
-        try:
-            token = kryptos.core.authenticate(
-                workspace=workspace, username=arguments["<name>"], password=arguments["<password>"]
+        kryptos_config[key] = str(arguments["<value>"])
+    elif arguments["reset"]:
+        kryptos_config = {}
+elif arguments["authenticate"]:
+    workspace = arguments["--workspace"] or kryptos_config.get("workspace")
+    if not workspace:
+        panic("No workspace defined")
+
+    try:
+        token = kryptos.core.authenticate(
+            workspace=workspace, username=arguments["<name>"], password=arguments["<password>"]
+        )
+    except httpx._exceptions.NetworkError:
+        panic("Failed to establish connection with Kryptos")
+
+    if not token:  # noqa
+        panic("Authentication failed")
+
+    kryptos_config["workspace"] = workspace
+    kryptos_config["token"] = token
+elif arguments["user"]:
+    token = arguments["--token"] or kryptos_config.get("token")
+    if not token:
+        panic("No token defined")
+
+    try:
+        if arguments["create"]:
+            kryptos.core.create_user(
+                token=token,
+                name=arguments["<name>"],
+                password=arguments["<password>"],
+                email_address=arguments["<email_address>"],
+                permissions=arguments["<permission"],
             )
-        except httpx._exceptions.NetworkError:
-            panic("Failed to establish connection with Kryptos")
+        elif arguments["delete"]:
+            kryptos.core.delete_user(token=token)
+    except httpx._exceptions.NetworkError:
+        panic("Failed to establish connection with Kryptos")
+elif arguments["entry"]:
+    pass
+elif arguments["namespace"]:
+    pass
+elif arguments["allow"]:
+    pass
+elif arguments["deny"]:
+    pass
 
-        if not token:  # noqa
-            panic("Authentication failed")
-
-        kryptos_config["workspace"] = workspace
-        kryptos_config["token"] = token
-    elif arguments["user"]:
-        token = arguments["--token"] or kryptos_config.get("token")
-        if not token:
-            panic("No token defined")
-
-        try:
-            if arguments["create"]:
-                kryptos.core.create_user(
-                    token=token,
-                    name=arguments["<name>"],
-                    password=arguments["<password>"],
-                    email_address=arguments["<email_address>"],
-                    permissions=arguments["<permission"],
-                )
-            elif arguments["delete"]:
-                kryptos.core.delete_user(token=token)
-        except httpx._exceptions.NetworkError:
-            panic("Failed to establish connection with Kryptos")
-    elif arguments["entry"]:
-        pass
-    elif arguments["namespace"]:
-        pass
-    elif arguments["allow"]:
-        pass
-    elif arguments["deny"]:
-        pass
-
-    with open(KRYPTOS_CONFIG, "w") as config_file:
-        json.dump(kryptos_config, config_file)
+with open(KRYPTOS_CONFIG, "w") as config_file:
+    json.dump(kryptos_config, config_file)
+"""
