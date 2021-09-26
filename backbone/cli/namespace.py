b54ebb06f6e1a61e8abc4b49017c10ff51043cda
David Al Mjali <david.al.mjali@gmail.com>
2021-09-26 15:10:25 -0700

    Implementing tree traversal

--- backbone/cli/namespace.py
+++ backbone/cli/namespace.py
@@ -10,14 +10,45 @@ from backbone.models import GrantAccess
 namespace_cli = typer.Typer()
 
 
+@namespace_cli.command("tree")
+def namespace_tree(key: str = typer.Argument(""), depth: int = 3):
+    """Visually display the store starting from a particular prefix"""
+    configuration = read_configuration()
+
+    def print_layer(client, namespace: str, level: int = 0, indentation: str = ""):
+        if level > depth:
+            return
+
+        resources = [(False, entry) for entry in client.namespace.get_child_entries(namespace)] + [
+            (True, namespace) for namespace in client.namespace.get_child_namespaces(namespace)
+        ]
+
+        for index, (is_namespace, resource) in enumerate(resources):
+            key: str = resource["key"]
+            next_final: bool = index == len(resources) - 1
+            prefix: str = "└──" if next_final else "├──"
+
+            typer.echo(f"{indentation}{prefix} {key}")
+            if is_namespace:
+                next_indentation = indentation + ("\t" if next_final else "│\t")
+                print_layer(client, key, level=level + 1, indentation=next_indentation)
+
+    try:
+        with client_from_config(configuration) as client:
+            print_layer(client, key)
+    except BackboneException as exc:
+        typer.echo(exc)
+        raise typer.Abort()
+
+
 @namespace_cli.command("list")
-def namespace_list(prefix: str = typer.Argument("")):
+def namespace_list(key: str = typer.Argument("")):
     """List namespaces with a given prefix"""
     configuration = read_configuration()
 
     try:
         with Halo("Listing namespaces"), client_from_config(configuration) as client:
-            namespaces = list(client.namespace.get_child_namespaces(prefix))
+            namespaces = list(client.namespace.get_child_namespaces(key))
 
         for namespace in namespaces:
             typer.echo(namespace["key"])
