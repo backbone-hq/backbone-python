bcc10411ce8900aa55142e501327a280b2fc3c67
David Al Mjali <david.al.mjali@gmail.com>
2021-10-03 07:58:39 -0700

    Removing unused crypto functions

--- backbone/crypto.py
+++ backbone/crypto.py
@@ -16,24 +16,6 @@ def derive_password_key(identity: str, password: str) -> bytes:
     return argon2id.kdf(size=32, password=password.encode(), salt=salt)
 
 
-def encrypt_with_secret(secret: bytes, plaintext: bytes) -> bytes:
-    return SecretBox(secret).encrypt(plaintext, encoder=encoding.URLSafeBase64Encoder)
-
-
-def encrypt_with_password(identity, password, plaintext) -> bytes:
-    password_key: bytes = derive_password_key(identity, password)
-    return encrypt_with_secret(password_key, plaintext)
-
-
-def decrypt_with_secret(secret: bytes, ciphertext: bytes) -> bytes:
-    return SecretBox(secret).decrypt(ciphertext, encoder=encoding.URLSafeBase64Encoder)
-
-
-def decrypt_with_password(identity, password, ciphertext) -> bytes:
-    password_key: bytes = derive_password_key(identity, password)
-    return decrypt_with_secret(password_key, ciphertext)
-
-
 def encrypt_grant(public_key: PublicKey, secret_key: PrivateKey) -> bytes:
     plaintext = secret_key.encode(encoder=encoding.RawEncoder)
     return Box(secret_key, public_key).encrypt(plaintext, encoder=encoding.URLSafeBase64Encoder)
